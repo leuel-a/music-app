13ede6b77d94b7792ed4d706c11702142e2773be
leuel-a <leuela1993@gmail.com>
2024-02-18 22:45:50 +0300

    add the x-refresh to the allowed headers

--- server/src/app.ts
+++ server/src/app.ts
@@ -12,7 +12,7 @@ const port = config.get<number>('port');
 app.use(function(req, res, next) {
   res.header("Access-Control-Allow-Origin", "*");
   res.header("Access-Control-Allow-Credentials", "true");
-  res.header("Access-Control-Allow-Headers", "Origin,Content-Type, Authorization, x-id, Content-Length, X-Requested-With");
+  res.header("Access-Control-Allow-Headers", "Origin,Content-Type, Authorization, x-id, Content-Length, X-Requested-With, x-refresh");
   res.header("Access-Control-Allow-Methods", "GET, POST, PUT, DELETE, OPTIONS");
   next();
 });
