76585b6141d84519f99dbaebcc276d98cfb3043b
leuel-a <leuela1993@gmail.com>
2024-02-18 22:23:43 +0300

    removed unneccessay files

--- server/build/src/middlewares/requireUser.js
+++
@@ -1,9 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-var requireUser = function (req, res, next) {
-    var user = res.locals.user;
-    if (!user)
-        return res.status(403).json({ message: 'Unauthorized' });
-    return next();
-};
-exports.default = requireUser;
