9799150562317ebb2fcba51c2f26b343eab08c66
leuel-a <leuela1993@gmail.com>
2024-02-18 22:40:54 +0300

    changed cors

--- server/src/app.ts
+++ server/src/app.ts
@@ -9,16 +9,11 @@ import deserializeUser from './middlewares/deserializeUser';
 const app = express();
 const port = config.get<number>('port');
 
-app.use(function (req, res, next) {
-  res.header('Access-Control-Allow-Origin', '*'); // update to match the domain you will make the request from
-  res.header(
-    'Access-Control-Allow-Headers',
-    'Origin, X-Requested-With, Content-Type, Accept, authorization, x-refresh',
-  );
-  res.header(
-    'Access-Control-Allow-Methods',
-    'GET, PUT, DELETE, POST',
-  );
+app.use(function(req, res, next) {
+  res.header("Access-Control-Allow-Origin", "*");
+  res.header("Access-Control-Allow-Credentials", "true");
+  res.header("Access-Control-Allow-Headers", "Origin,Content-Type, Authorization, x-id, Content-Length, X-Requested-With");
+  res.header("Access-Control-Allow-Methods", "GET, POST, PUT, DELETE, OPTIONS");
   next();
 });
 
